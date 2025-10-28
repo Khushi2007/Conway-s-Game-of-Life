afc9518493648a6e8d3de1e3c30e4c4f71a6c22a
Khushi2007 <kkg312007@gmail.com>
2025-10-29 01:19:13 +0530

    Latest commit

--- audio_manager.h
+++ audio_manager.h
@@ -5,14 +5,11 @@
 #include <stdbool.h>
 
 bool init_audio_system(void);
-
 bool play_background_music(const char* filename);
 void pause_background_music(void);
 void resume_background_music(void);
 void stop_background_music(void);
-
 bool play_sfx(const char* filename);
-
 void shutdown_audio_system(void);
 
 #endif
